b6f838b7779d08b20c42325134c2f39a3a775460
HookCreeping <baptista2017henrique@gmail.com>
2024-03-25 15:21:47 -0300

    problema de untracked file concertado

--- API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea624a2762339f22f2e5dc3a76a3e64011af8bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804b463c188a303f0d8cfc71336941afc3fbf3af")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
